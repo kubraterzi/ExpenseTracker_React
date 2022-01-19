17db647b7c5483ef5470a45e035d39103675c81f
Kübra Terzi <kubraterzi0303@gmail.com>
2022-01-19 19:10:00 +0300

    Added a composition component called Card for the UI.

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css";
+
+// Composition for border-radius and box-shadow
+function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>; // children is a reserved name(prop). The value of this prop will be the content between opening and closing tags of the custom component.
+}
+
+export default Card;
